374be3773b53e907a7aa1404e5ec82c36db288fb
ki33520 <ki33520@gmail.com>
2016-11-22 15:53:44 +0800

    add

--- desktop/js/main.js
+++ desktop/js/main.js
@@ -1,9 +1,39 @@
 "use strict";
 
-!function(w) {
-	var swiper = new Swiper('.swiper-container', {
-		pagination: '.swiper-pagination',
-		paginationClickable: true,
-		loop: true
-	});
-}(window);
+function setHTML(wrap,tpl,data){
+
+}
+
+$('.doctor-list').each(function(){
+	var self = this;
+	var url = $(this).data('json');
+	$.ajax({
+		url: url,
+		type: "get",
+		success: function(data){
+			$(self).empty();
+			$(data).each(function(index,item){
+				var li = $('<li class="item-doctor"><div class="item-inner"><div class="img-wrap"><img src="pic/'+item.pic+'" /></div></div></li>');
+				li.appendTo($(self));
+			})
+		}
+	})
+})
+
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        initialSlide: 0,
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        loop: true,
+        coverflow: {
+            rotate: 45,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows : true
+        }
+    });
